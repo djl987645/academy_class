fa80e475773bf40d70da893c3a02db6921dc9c86
종민 박 <djl987645@kyonggi.ac.kr>
2025-02-20 09:35:26 +0900

    HTML 파일 수정 및 Java 프로젝트 설정 추가

---
+++ class/JAVA/src/global/Basic_01.java
@@ -0,0 +1,8 @@
+package global;
+
+public class Basic_01 {
+    public static void main(String[] args) {
+        System.err.println("Hello World!");
+    }
+
+}
---
+++ class/JAVA/src/global/Basic_02.java
@@ -0,0 +1,15 @@
+package global;
+
+
+public class Basic_02 {
+    public static void main(String[] args) {
+        String[] str = {"두 번쨰 java 프로그램입니다", "콘솔 화면에 출력하고 있습니다."};
+        //첫 번째 방법
+        System.out.println(str[0]+" "+str[1]);
+
+        //두 번째 방법
+        System.out.println(str[0]);
+        System.out.println(str[1]);
+
+    }
+}
